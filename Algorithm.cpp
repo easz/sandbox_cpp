228a32d1aa1cba4f2edd5a5af4d40979c2ec48f7
Tsungi Lyn <ieqghamj9@mozmail.com>
2022-10-11 21:52:52 +0200

    algo

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -11,7 +11,7 @@
 /**
  * std::max_element
  * std::distance
-*/
+ */
 template <typename T>
 auto index_of_max(const std::vector<T>& v) {
   const auto iter_to_max = std::max_element(v.begin(), v.end());
@@ -21,18 +21,16 @@ auto index_of_max(const std::vector<T>& v) {
 /**
  * std::adjacent_find
  * std::not_equal_to
-*/
+ */
 template <typename T>
 bool all_equal(const std::vector<T>& v) {
   return std::adjacent_find(v.begin(), v.end(), std::not_equal_to<T>()) ==
          v.end();
 }
 
-
 namespace Algorithm {
 
 //
-
 
 int main() {
   const std::vector<int> numbers = {0, 2, 3, 1, 13, 5};
@@ -44,7 +42,8 @@ int main() {
   CHECK("all_equal", all_equal<int>({1, 1, 1}));
 
   const std::vector<int> another = {0, 2, 3, 4, 5, 6};
-  auto pair_to_mismatch = std::mismatch(numbers.begin(), numbers.end(), another.begin());
+  auto pair_to_mismatch =
+      std::mismatch(numbers.begin(), numbers.end(), another.begin());
   CHECK_VALUE("mismatch", 1, *pair_to_mismatch.first);
   CHECK_VALUE("mismatch", 4, *pair_to_mismatch.second);
 
